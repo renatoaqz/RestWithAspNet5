29e3d648672b7fe6e387b0d8e8ed26105402c052
Renato de Queiroz <renato.aqz@gmail.com>
2021-04-22 23:22:46 -0300

    adicionando Calculador

---
+++ 02_RestWithASPNET5_Calculadora/RestWithASPNET5/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+       
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstnumber}/{secondnumber}")]
+        public IActionResult Get(string firstnumber, string secondnumber)
+        {
+            if(IsNumeric(firstnumber) && IsNumeric(secondnumber))
+            {
+                var sum = Convert.ToDecimal(firstnumber) + Convert.ToDecimal(secondnumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strnumber)
+        {
+            double number;
+
+            return double.TryParse(strnumber, out number);
+        }
+    }
+}
