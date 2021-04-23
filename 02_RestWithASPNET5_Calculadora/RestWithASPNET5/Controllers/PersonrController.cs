90579ae2e4689effa09869438cf60b843bb6c8da
Renato Queiroz <renato.aqz@gmail.com>
2021-04-23 16:54:54 -0300

    Working with four verbs

---
+++ 02_RestWithASPNET5_Calculadora/RestWithASPNET5/Controllers/PersonrController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonrController : ControllerBase
+    {
+       
+        private readonly ILogger<PersonrController> _logger;
+
+        public PersonrController(ILogger<PersonrController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstnumber}/{secondnumber}")]
+        public IActionResult Sum(string firstnumber, string secondnumber)
+        {
+            if(IsNumeric(firstnumber) && IsNumeric(secondnumber))
+            {
+                var sum = Convert.ToDecimal(firstnumber) + Convert.ToDecimal(secondnumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string strnumber)
+        {
+            double number;
+
+            return double.TryParse(strnumber, out number);
+        }
+    }
+}
